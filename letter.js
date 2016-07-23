a2ed6da3f9703931af4f8e428e24fe8b6659e237
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2016-07-22 23:24:22 -0400

    changes

--- letter.js
+++
@@ -1,9 +0,0 @@
-// letter.js should control whether or not a letter appears as a "_" or as itself on-screen.
-// make a letter constructor
-// as a character property this.character = 'a'
-// parameters letter-> this.character
-// property guessedCorrectly boolean property this.guessedCorrectly
-// method letterDisplay 
-// if the letter is guessed correctly display character else display "_"
-
-
